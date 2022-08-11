bc0baa5dcd2c2270050b25901688e38c77dd7ad0
chelcee-xx <chelcee1614@gmail.com>
2022-08-11 19:12:15 +0100

    corrections made2

--- src/components/newsletter.jsx
+++ src/components/newsletter.jsx
@@ -1,17 +1,16 @@
 import news from "../img/footer_bg.png";
-import logo from "../img/logo.svg";
-import { Link } from "react-router-dom";
-import Footer from "./Footer";
+import React from "react";
 
 function Newsletter(){
     return(
+      <div className="wrap">
         <div className="newsletter">
         <img className="text-center" src={news} alt="a" />
 
         <div className="form-info">
           <h3>get our weekly update by subscribing to our newsletter</h3>
           <input type="text" /><input type="submit" defaultValue="Subscribe" />
-        </div></div>
+        </div></div>L</div>
     )
 }
 
